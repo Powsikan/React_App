eb7edcdb2a5f37cc5b314587f55e1e2840beb700
POWSIKAN <powsi1997@gmail.com>
2019-11-24 20:20:42 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Content from "./components/Content";
+import About from "./components/About";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div>
+            <Router>
+                <div>
+                    <NavBar></NavBar>
+                    <hr />
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+
+                        <Route path='/contact' component={Content} />
+
+                        <Route path='/about' component={About} />
+                    </Switch>
+                </div>
+            </Router>
+
+        </div>
+    );
+
+
+
+
 }
 
 export default App;
